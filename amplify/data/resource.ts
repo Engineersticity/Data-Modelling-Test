7ba0637b5cf5d37d15936a3333063d3046139a57
erastuskirui01 <erastuskirui01@gmail.com>
2024-11-28 08:01:06 +0000

    updated data schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -16,34 +16,20 @@ const schema = a.schema({
       enterTodo: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
-  Customer: a
+    Customer: a
     .model({
       customerId: a.id().required(),
-      // fields can be of various scalar types,
-      // such as string, boolean, float, integers etc.
       name: a.string(),
-      // fields can be of custom types
       location: a.customType({
-        // fields can be required or optional
         lat: a.float().required(),
         long: a.float().required(),
       }),
-      // fields can be enums
       engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
       collectionId: a.id(),
-      collection: a.belongsTo("Collection", "collectionId")
-      // Use custom identifiers. By default, it uses an `id: a.id()` field
+      collection: a.belongsTo("Collection", "collectionId"),
+      userProfile: a.hasOne("UserProfile", "customerId"), // Add this line
     })
     .identifier(["customerId"])
-    .authorization((allow) => [allow.publicApiKey()]),
-  Collection: a
-    .model({
-      customers: a.hasMany("Customer", "collectionId"), // setup relationships between types
-      tags: a.string().array(), // fields can be arrays
-      representativeId: a.id().required(),
-      // customize secondary indexes to optimize your query performance
-    })
-    .secondaryIndexes((index) => [index("representativeId")])
     .authorization((allow) => [allow.publicApiKey()]),
 
   UserProfile: a
@@ -55,6 +41,15 @@ const schema = a.schema({
       createdAt: a.datetime(),
       updatedAt: a.datetime()
     })
+    .authorization((allow) => [allow.publicApiKey()]),
+  Collection: a
+    .model({
+      customers: a.hasMany("Customer", "collectionId"), // setup relationships between types
+      tags: a.string().array(), // fields can be arrays
+      representativeId: a.id().required(),
+      // customize secondary indexes to optimize your query performance
+    })
+    .secondaryIndexes((index) => [index("representativeId")])
     .authorization((allow) => [allow.publicApiKey()]),
 
   Address: a.customType({
