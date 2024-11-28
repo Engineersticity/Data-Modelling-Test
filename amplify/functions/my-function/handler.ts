2d89e8916f7de11cd6c894ab2ef30a788606228b
erastuskirui01 <erastuskirui01@gmail.com>
2024-11-28 09:01:32 +0000

    return app id

--- amplify/functions/my-function/handler.ts
+++ amplify/functions/my-function/handler.ts
@@ -6,5 +6,5 @@ export const handler: Schema["myFunction"]["functionHandler"] = async (event) =>
     // arguments typed from `.arguments()`
     const { name } = event.arguments
     // return typed from `.returns()`
-    return `Hello, ${name}!`
+    return `Hello, ${name}!. This is the app id: ${amplifyAppId}`
 }
