ab1873c610f58db572f36eb4ad586e408fbfeaca
erastuskirui01 <erastuskirui01@gmail.com>
2024-11-28 09:11:44 +0000

    updated the variable to AWS_APP_ID

--- amplify/functions/my-function/handler.ts
+++ amplify/functions/my-function/handler.ts
@@ -1,7 +1,7 @@
 import type { Schema } from "../../data/resource"
 
 export const handler: Schema["myFunction"]["functionHandler"] = async (event) => {
-    const amplifyAppId = process.env.AWS_AMPLIFY_APP_ID;
+    const amplifyAppId = process.env.AWS_APP_ID;
     console.log('Amplify App ID:', amplifyAppId);
     // arguments typed from `.arguments()`
     const { name } = event.arguments
